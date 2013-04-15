a67525c416b0e2a5795e8197bbaf889cec35d931
Squirrel <fred96@live.ca>
2013-04-15 18:59:59 -0400

    added the base crafter

---
+++ tools_common/fred/Tools.java
@@ -0,0 +1,66 @@
+package fred;
+
+import items.ItemCrafter;
+import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
+
+@Mod(modid = "toolsMod", name="Tools", version="1.0.0")
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
+
+public class Tools
+{
+    @Instance
+    public static Tools instance;
+    
+    @SidedProxy(clientSide="fred.client.ClientProxy", serverSide="fred.CommonProxy")
+    public static CommonProxy proxy;
+    //items
+        //crafter
+        public static Item itemCrafter; 
+        //--crafter
+    //--items
+    
+    
+    
+    /**
+     * constructor
+     */
+    public Tools()
+    {
+        itemCrafter=new ItemCrafter(1000).setUnlocalizedName("CrafterTool").setCreativeTab(CreativeTabs.tabTools).setMaxStackSize(1);
+        LanguageRegistry.addName(itemCrafter, "Portable crafter");
+    }
+    
+    @PreInit
+    public void preInit(FMLPreInitializationEvent e)
+    {
+        
+    }
+    @Init
+    public void ini(FMLInitializationEvent e)
+    {
+        System.out.println("Tools mod ini...");
+        GameRegistry.addRecipe(new ItemStack(itemCrafter), "xxx", "xxx","xxx",'x',new ItemStack(Block.planks));
+    }
+    @PostInit
+    public void postInit(FMLPostInitializationEvent e)
+    {
+        
+    }
+    
+    
+}
