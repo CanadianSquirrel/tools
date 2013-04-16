f90a378533d565254e5f9309e9a1db7df7d29bd6
Squirrel <fred96@live.ca>
2013-04-16 09:55:56 -0400

    Made the crafter open a gui (id 0)

--- tools_common/fred/CommonProxy.java
+++ tools_common/fred/CommonProxy.java
@@ -1,11 +1,37 @@
 package fred;
 
-public class CommonProxy
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+import cpw.mods.fml.common.network.IGuiHandler;
+import fred.gui.GuiCrafter;
+
+public class CommonProxy implements IGuiHandler
 {
     public static String ITEMS_PNG = "/fred/items.png";
     
     public void registerRenderers()
     {
         
+    }
+
+    @Override
+    public Object getServerGuiElement(int ID, EntityPlayer player, World world,
+            int x, int y, int z)
+    {
+        // TODO Auto-generated method stub
+
+        return null;
+    }
+
+    @Override
+    public Object getClientGuiElement(int ID, EntityPlayer player, World world,
+            int x, int y, int z)
+    {
+        // TODO Auto-generated method stub
+        switch(ID)
+        {
+            case 0: return new GuiCrafter(player.inventory);
+        }
+        return null;
     }
 }
