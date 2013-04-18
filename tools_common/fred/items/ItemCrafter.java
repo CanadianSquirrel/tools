4717392689c6a2b259c516100530c29e02f8cef4
Squirrel <fred96@live.ca>
2013-04-18 17:13:03 -0400

    Does not open a gui anymore

--- tools_common/fred/items/ItemCrafter.java
+++ tools_common/fred/items/ItemCrafter.java
@@ -1,6 +1,5 @@
 package fred.items;
 
-import fred.Tools;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -15,7 +14,6 @@ public class ItemCrafter extends Item
     }
     public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
     {
-        par3EntityPlayer.openGui(Tools.instance, 0, par2World, (int)par3EntityPlayer.posX, (int)par3EntityPlayer.posY, (int)par3EntityPlayer.posZ);
         return par1ItemStack;
     }
 
